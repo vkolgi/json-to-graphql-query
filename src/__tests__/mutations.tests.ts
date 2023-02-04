02a6a416be252451e4f3caa826dad8d4876c7bac
Vinayak Kolagi <vkolagi@F95DHWRL22.local>
2023-02-04 16:31:48 +0530

    Added simple mutation test with a return param

--- src/__tests__/mutations.tests.ts
+++ src/__tests__/mutations.tests.ts
@@ -4,6 +4,23 @@ import { jsonToGraphQLQuery } from '../';
 
 describe('jsonToGraphQLQuery() - mutations', () => {
 
+    it('simple mutation', () => {
+        const mutation = {
+            mutation: {
+                delete_post: {
+                    __args: { id: 1234 },
+                    id: true,
+                }
+            } 
+        }
+        expect(jsonToGraphQLQuery(mutation, { pretty: true })).to.equal(
+        `mutation {
+    delete_post (id: 1234) {
+        id
+    }
+}`);
+    })
+
     it('correctly converts mutations with no specified return fields', () => {
         const query = {
             mutation: {
